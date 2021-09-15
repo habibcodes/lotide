9bc58c2b4ddeea2d7d30d3dd607609a3d36efcee
habib.codes <habib.codes@gmail.com>
2021-09-15 00:38:32 +0000

    added eqArrays to compare two arrrays

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+/*
+    - implement a function that can compare two arrays for a perfect match
+
+*/
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log(`Assertion Failed 🛑: [${actual}] !== [${expected}]`);
+  } else {
+    console.log(`Assertion Passed ✅: [${actual}] === [${expected}]`);
+  }
+};
+
+const eqArrays = (a, b) => {
+  if (a.length !== b.length) {
+    return false;
+  } else {
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
