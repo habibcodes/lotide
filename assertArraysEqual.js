056525e042d85b06318d9dd0cb9f7d13c7adc4ea
habibcodes <habib.codes@gmail.com>
2021-09-20 09:10:42 -0400

    refactored with modules and made test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -5,31 +5,13 @@
     - you can and should call the function below its declaration and visually ensure that the message printed to the console is accurate
 */
 
-const eqArrays = (a, b) => {
-  if (a.length !== b.length) {
-    return false;
-  } else {
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+// Import Modules
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (a, b) => {
-  //
-  // if (!eqArrays(a, b)) {
-  //   console.log('The two arrays are not identical.');
-  // } else {
-  //   console.log('The two arrays are identical.');
-  // }
-
   !eqArrays(a, b)
     ? console.log('🛑 The two arrays are not identical.')
     : console.log('✅ The two arrays are identical.');
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
+module.exports = assertArraysEqual;
