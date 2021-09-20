e8e0843f41ddbb26204ba0e3bca3ec6cf3f12e43
habibcodes <habib.codes@gmail.com>
2021-09-20 09:01:43 -0400

    refactored tail with modules and created test file

--- tail.js
+++ tail.js
@@ -1,23 +1,14 @@
 // tail - return last el in array //
 
-// FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  if (actual !== expected) {
-    console.log(`Assertion Failed 🛑: [${actual}] !== [${expected}]`);
-  } else {
-    console.log(`Assertion Passed ✅: [${actual}] === [${expected}]`);
-  }
-};
+// Import Module
+const assertEqual = require('./assertEqual');
 
+
+// Code Implementation
 const tail = (array) => {
   // must not mutate array
   return array.slice(1);
 };
 
-const result = tail(['Hello', 'Lighthouse', 'Labs']);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], 'Lighthouse'); // ensure first element is "Lighthouse"
-assertEqual(result[1], 'Labs'); // ensure second element is "Labs"
 
-const result2 = tail([]);
-assertEqual(result2, []);
+module.exports = tail;
